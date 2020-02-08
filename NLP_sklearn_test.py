5a5e6a41d3da7732c0438fc44b1ef1f2b80a5a26
Sophie Lee <44243849+gahyunlee@users.noreply.github.com>
2020-02-08 11:47:52 -0600

    Update NLP_sklearn_test.py

--- NLP_sklearn_test.py
+++ NLP_sklearn_test.py
@@ -1,1 +1,16 @@
 import numpy as np
+import pandas as pd
+
+# import data
+df = pd.read_csv('smsspamCollection.tsv', sep='\t')
+print(df.head())
+
+print(len(df))
+
+# check for missing values:
+print(df.isnull().sum()) #if there is 1 then there is a missing value.
+
+# take a quick look at the ham and spam label column
+df['label'].unique()
+
+df['label'].value_counts()
